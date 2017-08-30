aed4920495d8fef73af3ce085edcf84cea0092e0
Unknown <sbecker@methodinthemadness.eu>
2017-08-30 22:18:19 +0200

    Diagramme buteurs.

--- client/app/playerviewer-component/playerviewer-component.ts
+++ client/app/playerviewer-component/playerviewer-component.ts
@@ -29,6 +29,7 @@ export class PlayerViewerComponent implements OnInit {
     public player: IPlayer;
     public teamLogo: string = "";
 
+    // Données pour les performances chiffrées du joueurs
     public performancesData: ChartData;
     public performancesType: string = "";
     public performancesOptions:any = {
@@ -43,14 +44,23 @@ export class PlayerViewerComponent implements OnInit {
         }
     };
 
+    // Données pour le donut de présentéisme du joueur.
     public presenceData: SimpleChartData;
     public presenceType: string = "";
     public presenceOptions: any = {
         donut: true,
-        donutWidth: 60,
+        donutWidth: 20,
         startAngle: 270,
-        total: 200,
+        total: 0,
         showLabel: false
+    };
+
+    // Données pour le graph de buts cumulés
+    public goalsData: ChartData;
+    public goalsType: string = "";
+    public goalsOptions: any = {
+        fullWidth: true,
+        lineSmooth: Chartist.Interpolation.none()
     };
 
     constructor( private playerService: PlayerService, private route: ActivatedRoute  ) {
@@ -63,6 +73,11 @@ export class PlayerViewerComponent implements OnInit {
         };
 
         this.presenceData = {
+            labels: [],
+            series: []
+        };
+
+        this.goalsData = {
             labels: [],
             series: []
         };
@@ -94,6 +109,9 @@ export class PlayerViewerComponent implements OnInit {
         let daily: number[] = [];
         let average: number[] = [];
 
+        let goalForThisDay: number[] = [];
+        let cumulativeGoals: number[] = [];
+
         let minutesAsPlayer: number = 0;
         let minutesAsSubstitute: number = 0;
         let minutesOnTheBench: number = 0;
@@ -104,23 +122,33 @@ export class PlayerViewerComponent implements OnInit {
 
         let performance: IPerformance;
 
+
         for( performance of this.player.performances ) {
 
+            // On stocke le nombre de performance et la somme de toutes les performances
+            // Cette dernière nous permettant de calculer à chaque fois la moyenne au jour donné.
             numberOfPerformances++;
             totalRateValue += performance.rate;
 
+            // On stocke la perf du joueur, ainsi que la moyenne du jour
             daily[ performance.day - 1 ] = performance.rate;
             average[ performance.day - 1 ] = totalRateValue / numberOfPerformances;
 
             if( performance.day > lastDay ) lastDay = performance.day;
 
+            // Si on était remplaçant, ce sont des minutes de remplaçant
+            // Sinon ce sont des minutes de titulaires.
             if( performance.sub ) {
                 minutesAsSubstitute += performance.minutes;
             } else {
                 minutesAsPlayer += performance.minutes;
             }
 
+            // Et le reste ce sont les minutes "on était pas là"
             minutesOnTheBench = 90 - performance.minutes;
+
+            // On stocke les buts du jour
+            goalForThisDay[ performance.day - 1 ] = performance.goalFor;
         }
 
         // On fait une deuxième passe pour les jours qui manquent, histoire d'être sur
@@ -130,7 +158,31 @@ export class PlayerViewerComponent implements OnInit {
             if( !daily[ i ] ) {
                 daily[ i ] = null;
                 average[ i ] = null;
+
+                // S'il n'y a pas de performances, ça veut dire que le type est resté sur le banc
+                // Voir à la maison devant BeIn à regarder le match.
+                minutesOnTheBench += 90;
             }
+
+            // Par défaut on dit qu'on a marqué que dalle
+            let todayGoal: number = 0;
+
+            // On ajuste le compteur de but si le mec a joué
+            // Là il peut quand même toujours avoir marqué zéro
+            if( goalForThisDay[ i ] ) {
+                todayGoal = goalForThisDay[ i ];
+            }
+
+            // On part du principe qu'on a pas marqué les jours précédent
+            let previousDayGoal: number = 0;
+
+            // Si c'est pas le premier jouer, on va récupérer la valeur
+            if( i > 0 ) {
+                previousDayGoal = cumulativeGoals[ i - 1 ];
+            }
+
+            // Et on stocke le tout
+            cumulativeGoals[ i ] = previousDayGoal + todayGoal;
         }
 
         // On construit la série de valeurs
@@ -144,9 +196,17 @@ export class PlayerViewerComponent implements OnInit {
 
         // On construit notre affichage de présence
         this.presenceData.series = [ minutesOnTheBench, minutesAsSubstitute, minutesAsPlayer ];
+        this.presenceOptions.total = 2 * ( minutesAsPlayer + minutesAsSubstitute + minutesOnTheBench );
+
+        // On construit notre affichage de but
+        this.goalsData.series = [ cumulativeGoals ];
+        this.goalsData.labels = this.performancesData.labels;
+
+        console.log( this.goalsData );
 
         // On paramètre le type de diagramme (pour déclencher le refresh, sinon le diagramme ne s'affiche pas)
         this.performancesType = "Line";
         this.presenceType = "Pie";
+        this.goalsType = "Line";
     }
 }
