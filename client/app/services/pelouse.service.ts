6f874b49972be4bd8a928d16b41121209bc034d7
Unknown <sbecker@methodinthemadness.eu>
2017-11-06 20:09:10 +0100

    fix(pelouse): tslintazion

--- client/app/services/pelouse.service.ts
+++ client/app/services/pelouse.service.ts
@@ -1,7 +1,7 @@
 import * as _ from "lodash";
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/Observable";
-import {Headers,Http,Response} from '@angular/http';
+import {Headers, Http, Response} from '@angular/http';
 import {ILeagueMPG, IPlayerMercatoMPG, IUserMPG} from "../../../shared/models/pelouse";
 import {AsyncSubject} from "rxjs/AsyncSubject";
 
@@ -23,15 +23,15 @@ export class PelouseService {
         // On essaye de récupérer les infos depuis le local storage.
         // Cela nous évite de faire une requête pour le signIn si on a encore
         // le token d'identification.
-        let user = localStorage.getItem( 'currentUser' );
-        if( user ) {
-            this.user = JSON.parse( user );
+        let user = localStorage.getItem('currentUser');
+        if (user) {
+            this.user = JSON.parse(user);
             this.isLoggedIn = true;
         }
         else {
             this.isLoggedIn = false;
         }
-        this.logged.next( this.isLoggedIn );
+        this.logged.next(this.isLoggedIn);
         this.logged.complete();
     }
 
@@ -45,51 +45,50 @@ export class PelouseService {
 
     public getLeagues(): Observable<ILeagueMPG[]> {
 
-        let headers: Headers = new Headers({'Authorization': this.user.token});
+        let headers: Headers = new Headers({Authorization: this.user.token});
 
-        return this.http.get( this.dashboardApi, { headers: headers })
-            .map( (response: Response) => {
+        return this.http.get(this.dashboardApi, {headers: headers})
+            .map((response: Response) => {
                 let data: any = response.json().data;
-                console.log(data);
                 return data.leagues;
             });
     }
 
-    public getPlayersAvailableForLeague( league: string ): Observable<string[]> {
+    public getPlayersAvailableForLeague(league: string): Observable<string[]> {
 
-        let headers: Headers = new Headers({'Authorization': this.user.token});
+        let headers: Headers = new Headers({Authorization: this.user.token});
 
-        return this.http.get( this.mercatoApi + league + this.mercatoApiSecondPart, { headers: headers })
-            .map( (response: Response) => {
+        return this.http.get(this.mercatoApi + league + this.mercatoApiSecondPart, {headers: headers})
+            .map((response: Response) => {
                 let players: IPlayerMercatoMPG[] = response.json().availablePlayers;
-                return _.map( players, (player:IPlayerMercatoMPG) => player.id.slice( 7 ));
+                return _.map(players, (player: IPlayerMercatoMPG) => player.id.slice(7));
             })
 
     }
 
-    public login( login: string, password: string ): Observable<IUserMPG>  {
+    public login(login: string, password: string): Observable<IUserMPG> {
 
-        return this.http.post( this.loginApi, {
+        return this.http.post(this.loginApi, {
             email: login,
             password: password,
             language: "fr-FR"
         })
-        .map((response) => response.json()).map( (response) => {
-            let user:any = response;
-            if( user && user.token ) {
-                localStorage.setItem( 'currentUser', JSON.stringify(user));
-                this.user = user;
-                this.isLoggedIn = true;
-                this.logged.next( this.isLoggedIn );
-                this.logged.complete();
-            }
+            .map((response) => response.json()).map((response) => {
+                let user: any = response;
+                if (user && user.token) {
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                    this.user = user;
+                    this.isLoggedIn = true;
+                    this.logged.next(this.isLoggedIn);
+                    this.logged.complete();
+                }
 
-            return user;
-        });
+                return user;
+            });
     }
 
     public logout() {
         // On vire les données utilisateurs de la machine
-        localStorage.removeItem( 'currentUser' );
+        localStorage.removeItem('currentUser');
     }
 }
